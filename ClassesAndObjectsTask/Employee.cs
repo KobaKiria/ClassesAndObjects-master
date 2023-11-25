7fe349fc4d72838df3b3dc7795be97da829a9c51
Thelion11 <kobakiria123@gmail.com>
2023-11-25 13:07:39 +0400

    initial comit

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname { get; set; }
+        private int age { get; set; }
+
+        public Employee()
+        {
+               
+        }
+
+        public Employee(string Surname, int Age)
+        {
+            surname = Surname;
+            age = Age;
+        }
+
+        private string GetAgeInSring()
+        {
+            return Convert.ToString(age);
+        }
+        public void Set_Surname(string newName) 
+        {
+            surname = newName;
+        }
+        public string Get_Emploee_Info()
+        {
+            return $"surname: {surname}, Age: {GetAgeInSring()}";
+        }
+    }
+}
